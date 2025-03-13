525f609ce20c52e44daeb0ad5a85ca8d5a929b42
Cliffochi <cliffhani4@gmail.com>
2025-03-13 10:00:34 +0300

    first question SQL code

--- sql_load-20250219T081109Z-001/project_sql/1_top_paying_jobs.sql
+++ sql_load-20250219T081109Z-001/project_sql/1_top_paying_jobs.sql
@@ -4,3 +4,22 @@ Question: What are the top-paying data analyst jobs?
 - Focuses on job postings with specified salaries (remove nulls).
 - Why? Highlight the top-paying opportunities for Data Analysts, offering insights into emp
 */
+
+SELECT
+    job_id,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date,
+    name AS company_name
+FROM
+    job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE
+    job_title_short = 'Data engineer' AND --- can be tailored to a specific location and job
+    job_location = 'Kenya' AND
+    salary_year_avg IS NOT NULL
+ORDER BY
+    salary_year_avg DESC
+LIMIT 10
